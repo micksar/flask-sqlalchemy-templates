51176dc623e5e2eb20ebf1940ea3083ba115b57f
Mike <saragasmixalis@gmail.com>
2023-05-01 10:11:35 +0300

    Not Finished data importing via mysqlAlchemy

--- App.py
+++ App.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
-
-
-
-
-
-
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.secret_key = "secret key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:test@mysql_container/crud_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id =    db.Column(db.Integer, primary_key=True)
+    name =  db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    #Class constructor
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
 
 @app.route('/')
 def Index():
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Commands:
 Run the container to start the db:
 **docker run -d -p 3306:3306 --name mysql_container -e MYSQL_ROOT_PASSWORD=test mysql**
+**docker inspect mysql_container | grep IPAddress**
+**mysql -h <mysql_container_ip> -P 3306 -u root -p<db_password> -e "CREATE DATABASE crud_db;"**
 
 Start the virtual environment:
 **source venv/bin/activate**
